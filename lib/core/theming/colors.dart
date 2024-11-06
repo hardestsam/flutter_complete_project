59ed5f01d2339e6bfdb28cd3a666fb7e11d8bd82
hardestsam <s.sanabani@gmail.com>
2024-11-07 01:54:46 +0300

    added some colors & TextStyles.

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 class ColorsManager{
 
   static const Color mainBlue = Color(0xFF247CFF);
+  static const Color darkBlue = Color(0xFF242424);
   static const Color gray = Color(0xFFE5E5E5);
-
+  static const Color lightGray = Color(0xFFC2C2C2);
+  static const Color lighterGray = Color(0xFFEDEDED);
+  static const Color moreLightGray = Color(0xFFFDFDFF);
 }
