30fff9b53d4553e81e16909cf1847f149ee11b0e
hardestsam <s.sanabani@gmail.com>
2024-11-29 18:09:54 +0300

    added Login-Api
    added di & added AppRegex & integrated with Login EP & added Login Repo And Cubit & added Login State and Validation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_complete_project/core/di/dependency_injection.dart';
 import 'package:flutter_complete_project/core/routing/routes.dart';
+import 'package:flutter_complete_project/features/home/ui/home_screen.dart';
+import 'package:flutter_complete_project/features/login/logic/cubit/login_cubit.dart';
 import 'package:flutter_complete_project/features/onboarding/onboarding_screen.dart';
 import 'package:flutter_complete_project/features/login/ui/login_screen.dart';
+import 'package:flutter_complete_project/features/sign_up/logic/sign_up_cubit.dart';
+import 'package:flutter_complete_project/features/sign_up/ui/sign_up_screen.dart';
 
 class AppRouter {
-
-
   Route generateRoute(RouteSettings settings){
   //this arguments to be passed in any screen like this (arguments as ClassName)
     final arguments = settings.arguments;
@@ -18,7 +22,21 @@ class AppRouter {
        );
      case Routes.loginScreen:
        return MaterialPageRoute(
-         builder:  (_) => const LoginScreen(),
+         builder:  (_) =>  BlocProvider(
+           create: (context) => getIt<LoginCubit>(),
+           child: const LoginScreen(),
+         ),
+       );
+     case Routes.signUpScreen:
+       return MaterialPageRoute(
+         builder: (_) => BlocProvider(
+           create: (context) => getIt<SignupCubit>(),
+           child: const SignupScreen(),
+         ),
+       );
+     case Routes.homeScreen:
+       return MaterialPageRoute(
+         builder:  (_) =>  const HomeScreen(),
        );
      default:
        return MaterialPageRoute(
